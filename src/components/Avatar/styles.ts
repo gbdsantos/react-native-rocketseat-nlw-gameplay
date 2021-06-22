e522e2dfa3a673d3603fe4175fcf29950330465a
Guilherme Bezerra <gbsantos.it@gmail.com>
2021-06-22 16:42:18 -0300

    :sparkles: feature(mobile): create avatar component

---
+++ src/components/Avatar/styles.ts
@@ -0,0 +1,19 @@
+import { StyleSheet } from 'react-native'; 
+
+export const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    justifyContent: 'center',
+
+    borderRadius: 8,
+    marginRight: 22,
+    height: 53,
+    width: 53,
+  },
+
+  avatar: {
+    borderRadius: 8,
+    height: 46,
+    width: 46,
+  }
+})
