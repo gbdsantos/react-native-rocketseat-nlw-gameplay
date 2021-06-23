7bd9d46a21ed6aca353b69bba92f6cd913f9eea8
Guilherme Bezerra <gbsantos.it@gmail.com>
2021-06-23 15:55:22 -0300

    :sparkles: feature(mobile): create ListHeader component

---
+++ src/components/ListHeader/styles.ts
@@ -0,0 +1,23 @@
+import { StyleSheet } from "react-native";
+import { theme } from "../../global/styles/theme";
+
+export const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingHorizontal: 24,
+    width: '100%',
+  },
+
+  title: {
+    color: theme.colors.heading,
+    fontFamily: theme.fonts.title700,
+    fontSize: 18 
+  },
+
+  subtitle: {
+    color: theme.colors.highlight,
+    fontFamily: theme.fonts.text400,
+    fontSize: 13 
+  }
+})
