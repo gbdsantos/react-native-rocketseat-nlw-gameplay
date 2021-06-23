dd5f68795bc3fee57625ec4d841e6de1b19c2835
Guilherme Bezerra <gbsantos.it@gmail.com>
2021-06-23 15:57:18 -0300

    :star: feature(mobile): create Appointment component

---
+++ src/components/Appointment/styles.ts
@@ -0,0 +1,65 @@
+import { StyleSheet } from "react-native";
+import { theme } from "../../global/styles/theme";
+
+export const styles = StyleSheet.create({
+  container: {
+    alignSelf: 'center',
+    flexDirection: 'row', 
+    width: '100%',
+  },
+
+  content: {
+    flex: 1,
+  },
+
+  header: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginBottom: 12,
+    width: '100%',
+  },
+
+  title: {
+    color: theme.colors.heading,
+    fontFamily: theme.fonts.title700,
+    fontSize: 18 
+  },
+
+  category: {
+    color: theme.colors.highlight,
+    fontFamily: theme.fonts.text400,
+    fontSize: 13,
+    marginRight: 24,
+  },
+
+  footer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: '100%',
+  },
+
+  dateInfo: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+
+  playersInfo: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+
+  date: {
+    color: theme.colors.heading,
+    fontFamily: theme.fonts.text500,
+    fontSize: 13,
+    marginLeft: 7,
+  },
+
+  player: {
+    fontFamily: theme.fonts.text500,
+    fontSize: 13,
+    marginLeft: 7,
+    marginRight: 24,
+  }
+
+})
