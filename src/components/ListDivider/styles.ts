5105e9774c4ab3159ba7f8b2eb295e63f2869d5f
Guilherme Bezerra <gbsantos.it@gmail.com>
2021-06-23 15:57:44 -0300

    :nail_care: style(mobile): create ListDivider component
    - Separe list of items.

---
+++ src/components/ListDivider/styles.ts
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+import { theme } from "../../global/styles/theme";
+
+export const styles = StyleSheet.create({
+  container: {
+    alignSelf: 'flex-end',
+    backgroundColor: theme.colors.secondary40,
+    marginVertical: 21,
+    height: 1,
+    width: '80%'
+  }
+});
