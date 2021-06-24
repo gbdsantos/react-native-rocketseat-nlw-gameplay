15c07f62531b2d440c43f24e39c4f912137c2f52
Guilherme Bezerra <gbsantos.it@gmail.com>
2021-06-24 00:18:30 -0300

    :gem: feature(mobile): finish AppointmentDetails screen

--- src/screens/AppointmentDetails/index.tsx
+++ src/screens/AppointmentDetails/index.tsx
@@ -1,15 +1,36 @@
 import React from 'react';
-import { View } from 'react-native';
+import { FlatList, ImageBackground, Text, View } from 'react-native';
 import { Fontisto } from '@expo/vector-icons';
 import { BorderlessButton } from 'react-native-gesture-handler';
 
 import { Background } from '../../components/Background';
+import { ButtonIcon } from '../../components/ButtonIcon';
 import { Header } from '../../components/Header';
+import { ListDivider } from '../../components/ListDivider';
+import { ListHeader } from '../../components/ListHeader';
+import { Member } from '../../components/Member';
 
+import BannerImg from '../../assets/banner.png'
 import { styles } from './styles';
 import { theme } from '../../global/styles/theme';
 
 export function AppointmentDetails() {
+  const members = [
+    {
+      id: '1',
+      username: 'Guilherme',
+      avatar_url: 'https://github.com/gbdsantos.png',
+      status: 'online'
+    },
+
+    {
+      id: '2',
+      username: 'Victor',
+      avatar_url: 'https://github.com/grossiv.png',
+      status: 'ocupado'
+    },
+  ]
+  
   return (
     <Background>
       <Header 
@@ -24,6 +45,41 @@ export function AppointmentDetails() {
         }
         title="Detalhes"
       />
+
+      <ImageBackground 
+        source={BannerImg}
+        style={styles.banner}
+      >
+
+        <View style={styles.bannerContent}>
+          <Text style={styles.title}>
+            Lendários
+          </Text>
+
+          <Text style={styles.subtitle}>
+            É hoje que vamos chegar ao challenger sem perder uma partida da md10
+          </Text>
+        </View>
+      </ImageBackground>
+
+      <ListHeader 
+        title="Jogadores"
+        subtitle="Total 2"
+      />
+
+      <FlatList 
+        data={members}
+        ItemSeparatorComponent={() => <ListDivider />}
+        keyExtractor={item => item.id}
+        renderItem={({ item }) => (
+          <Member data={item} />
+        )}
+        style={styles.members}
+      />
+
+      <View style={styles.footer}>
+        <ButtonIcon title="Entrar na partida" />
+      </View>
     </Background>
   );
 }
--- src/screens/AppointmentDetails/styles.ts
+++ src/screens/AppointmentDetails/styles.ts
@@ -1,7 +1,47 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet } from 'react-native';
+import { getBottomSpace } from 'react-native-iphone-x-helper';
+
+import { theme } from "../../global/styles/theme";
 
 export const styles = StyleSheet.create({
   container: {
-    
+    flex: 1,
+  },
+
+  banner: {
+    marginBottom: 30,
+    height: 234,
+    width: '100%',
+  },
+
+  bannerContent: {
+    flex: 1,
+    justifyContent: 'flex-end',
+
+    marginBottom: 30,
+    paddingHorizontal: 24,  
+  },
+
+  title: {
+    color: theme.colors.heading,
+    fontFamily: theme.fonts.title700,
+    fontSize: 28,
+  },
+
+  subtitle: {
+    color: theme.colors.heading,
+    fontFamily: theme.fonts.text400,
+    fontSize: 13,
+  },
+
+  members: {
+    marginLeft: 24,
+    marginTop: 27,
+  },
+
+  footer: {
+    marginBottom: getBottomSpace(),
+    paddingHorizontal: 24,
+    paddingVertical: 20,
   }
 });
