72abfbdeccd2734fa27558d53b23d6655aa17a9f
Guilherme Bezerra <gbsantos.it@gmail.com>
2021-06-24 01:54:06 -0300

    :sparkles: feature(mobile): create Button and TextArea components

---
+++ src/components/Button/styles.ts
@@ -0,0 +1,36 @@
+import { StyleSheet } from 'react-native';
+import { theme } from '../../global/styles/theme';
+
+export const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    alignItems: 'center',
+
+    backgroundColor: theme.colors.primary,
+    borderRadius: 8,
+    height: 56,
+    width: '100%',
+  },
+
+  title: {
+    flex: 1,
+    color: theme.colors.heading,
+    fontFamily: theme.fonts.text500,
+    fontSize: 15,
+    textAlign: 'center'
+  },
+
+  iconWrapper: {
+    alignItems: 'center',
+    borderColor: theme.colors.line,
+    borderRightWidth: 1,
+    justifyContent: 'center',
+    height: 56,
+    width: 56
+  },
+
+  icon: {
+    height: 18,
+    width: 24,
+  }
+});
