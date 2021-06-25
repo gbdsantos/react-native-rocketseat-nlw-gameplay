bb2ee520871718ecb40abe26a5ac52ac8ee65f1e
Guilherme Bezerra <gbsantos.it@gmail.com>
2021-06-25 10:51:51 -0300

    :nail_care: refactor(mobile): fix elements position on Guilds screen

--- src/screens/Guilds/index.tsx
+++ src/screens/Guilds/index.tsx
@@ -24,15 +24,52 @@ export function Guilds({ handleGuildSelect }: Props) {
       name: 'Clã dos Magos',
       icon: 'image.png',
       owner: false,
-    }
+    },
+
+    {
+      id: '3',
+      name: 'Clã dos Magos',
+      icon: 'image.png',
+      owner: false,
+    },
+
+    {
+      id: '4',
+      name: 'Clã dos Magos',
+      icon: 'image.png',
+      owner: false,
+    },
+
+    {
+      id: '5',
+      name: 'Clã dos Magos',
+      icon: 'image.png',
+      owner: false,
+    },
+
+    {
+      id: '6',
+      name: 'Clã dos Magos',
+      icon: 'image.png',
+      owner: false,
+    },
+
+    {
+      id: '7',
+      name: 'Clã dos Magos',
+      icon: 'image.png',
+      owner: false,
+    },
   ]
   
   return(
     <View style={styles.container}>
       <FlatList 
+        contentContainerStyle={{ paddingBottom: 68, paddingTop: 104 }}
         data={guilds}
-        ItemSeparatorComponent={() => <ListDivider />}
+        ItemSeparatorComponent={() => <ListDivider isCentered />}
         keyExtractor={item => item .id}
+        ListHeaderComponent={() => <ListDivider isCentered />}
         renderItem={({ item }) => (
           <Guild 
             data={item} 
