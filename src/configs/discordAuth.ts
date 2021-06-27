bcfcaca144521c031cb009e2e1e03032ebc952de
Guilherme Bezerra <gbsantos.it@gmail.com>
2021-06-27 03:13:29 -0300

    :wrench: config(mobile): add envinroment variables of the discord
    - Create discord auth variables;
    - Create environment variables index.

---
+++ src/configs/discordAuth.ts
@@ -0,0 +1,13 @@
+const REDIRECT_URI = 'https%3A%2F%2Fauth.expo.io%2F%40anonymous%2Freact-native-rocketseat-nlw-gameplay-5dff293c-3d37-4fd6-a6b6-39fc50a78cc5';
+const SCOPE = 'identify%20email%20connections%20guilds'; 
+const RESPONSE_TYPE = 'token';
+const CLIENT_ID = '858514023070695454';
+const CDN_IMAGE = 'https://cdn.discordapp.com';
+
+export {
+  REDIRECT_URI,
+  SCOPE,
+  RESPONSE_TYPE,
+  CLIENT_ID,
+  CDN_IMAGE
+}
